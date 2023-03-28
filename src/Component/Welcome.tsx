c6322dba57f6e2d0d33f3d95d5bce1a5af46f1b5
pyshen <pyshen.watson@gmail.com>
2023-03-28 16:36:57 +0800

    Seperate everything to components

---
+++ src/Component/Welcome.tsx
@@ -0,0 +1,10 @@
+export default function Welcome(props:any){
+
+    return (
+        <>
+            <h1> This is the normal HTML syntax you know </h1>
+            <h2> My name is {props.name} </h2>
+        </>
+    )
+}
+
