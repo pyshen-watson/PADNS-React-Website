288d5a0c4a16f3da245a0949e49bcbba5a3a858b
pyshen <pyshen.watson@gmail.com>
2023-03-31 13:34:54 +0800

    Add 5 pages, use better route method

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,36 @@
 import './App.css'
-import Welcome from './Component/Welcome'
-import Clickme from './Component/Clickme'
-import Counter from './Component/Counter'
+import { NavLink, Routes, Route } from 'react-router-dom'
+import Index from './Pages'
+import About from './Pages/about'
+import Works from './Pages/works'
+import CV from './Pages/cv'
+import Contact from './Pages/contact'
 
 function App() {
 
-  const name = 'Elliot'
+  const PageList = [
+    { name: "Index", path: "/", element: <Index /> },
+    { name: "About", path: "/about", element: <About /> },
+    { name: "Works", path: "/works", element: <Works /> },
+    { name: "CV", path: "/cv", element: <CV /> },
+    { name: "Contact", path: "/contact", element: <Contact /> }
+  ]
+
 
   return (
-    <div>
-      <Welcome name={name}/>
-      <Clickme/>
-      <Counter/>
-    </div>
+    <>
+      <nav className='navbar'>
+        {
+          PageList.map(page =>
+            <div className="link">
+              <NavLink to={page.path}>{page.name}</NavLink>
+            </div>)
+        }
+      </nav>
+      <Routes>
+        {PageList.map(page => <Route path={page.path} element={page.element} />)}
+      </Routes>
+    </>
   )
 }
 
