29b9fc7256ad5a7f253244e621b96f527ac6ae99
pyshen <pyshen.watson@gmail.com>
2023-04-01 10:10:49 +0800

    Build the bewsite

--- src/Data/WebsiteList.ts
+++ src/Data/WebsiteList.ts
@@ -1,32 +1,40 @@
+import pyshen from '@Assets/pyshen.png'
+import ntucc from '@Assets/ntucc.png'
+import dennis from '@Assets/dennis.png'
+import ecp from '@Assets/ecp.png'
+import protectwither from '@Assets/protectwither.png'
+import presshian from '@Assets/presshian.png'
+
+
 export default [
     {
         site_name: "Pyshen",
-        site_image: "src/Assets/pyshen.png",
+        site_image: pyshen,
         site_url: "https://pyshen.net/"
     },
     {
         site_name: "臺大計中 教學組",
-        site_image: "src/Assets/ntucc.png",
+        site_image: ntucc,
         site_url: "https://webpageprodvm.ntu.edu.tw/ntuccelearning/"
     },
     {
         site_name: "大法官 湯德宗",
-        site_image: "src/Assets/dennis.png",
+        site_image: dennis,
         site_url: "https://dennis-tang.site/"
     },
     {
         site_name: "玩轉極限",
-        site_image: "src/Assets/ecp.png",
+        site_image: ecp,
         site_url: "https://extremecubing.com/"
     },
     {
         site_name: "成忘老太太",
-        site_image: "src/Assets/protectwither.png",
+        site_image: protectwither,
         site_url: "https://protectwither-tang.net/"
     },
     {
         site_name: "Presshian Studio",
-        site_image: "src/Assets/presshian.png",
+        site_image: presshian,
         site_url: "https://presshian.com/"
     },
 ]
